dd61f462967c44d7977ed1fa751b74035ded3b68
Ulrich Spörlein <uqs@FreeBSD.org>
2021-02-09 10:22:07 +0100

    Disable Via so that 7 layers work, rename some things.

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/uqs/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/uqs/rules.mk
@@ -1,5 +1,5 @@
 RGBLIGHT_ENABLE = yes
-VIA_ENABLE = yes
+#VIA_ENABLE = yes  # doesn't fit with 7 layers :/
 #BOOTMAGIC_ENABLE = lite
 LTO_ENABLE = yes  # disables the legacy TMK Macros and Functions features
 
