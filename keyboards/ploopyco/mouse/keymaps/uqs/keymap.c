96861b7c4f1fd3a5fa77d937daef9538c4b64aa2
Ulrich Spörlein <uqs@FreeBSD.org>
2021-03-12 14:42:25 +0100

    Stardew Valley layer with 2x right click plus 'n'
    Except, that doesn work in windows, but xev(1) shows everything
    properly. Hrm.

--- keyboards/ploopyco/mouse/keymaps/uqs/keymap.c
+++ keyboards/ploopyco/mouse/keymaps/uqs/keymap.c
@@ -17,6 +17,26 @@
  */
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+  DBL_CLK_NO = SAFE_RANGE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case DBL_CLK_NO:
+      if (record->event.pressed) {
+        SEND_STRING(SS_TAP(X_BTN2) SS_DELAY(300) SS_TAP(X_BTN2) SS_DELAY(300) SS_TAP(X_N));
+      }
+      return false;
+  }
+  return true;
+}
+
+
+// Layout is:
+// left-most, M1, M3, M2, right-most, fwd, back (on side), tiny middle one
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(KC_LCTL, KC_BTN1, KC_BTN3, KC_BTN2, KC_LSFT, KC_BTN4, KC_BTN5, KC_LGUI),
+    [0] = LAYOUT(KC_LCTL, KC_BTN1, KC_BTN3, KC_BTN2, KC_LSFT, KC_BTN4, KC_BTN5, DF(1)),  // shooters
+    [1] = LAYOUT(KC_1, KC_BTN1, KC_Y, KC_BTN2, DBL_CLK_NO, KC_N, KC_Y, DF(0)),        // stardew valley, sword on 1
 };
