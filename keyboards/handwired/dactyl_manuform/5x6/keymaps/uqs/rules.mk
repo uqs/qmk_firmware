caf664bc049a40e83d50576fb8528a3a333bb74f
Ulrich Spörlein <uqs@FreeBSD.org>
2021-06-10 12:51:58 +0200

    OSM mods on the EXTD layer and getting rid of trilayer state
    This will allow to use the opposite thumb when in certain layers, for
    example to have "0" on the left thumb when the right has activated the
    NUM layer.
    
    Though I might use it to be able to hold ESC or Space or Backspace

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/uqs/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/uqs/rules.mk
@@ -7,5 +7,5 @@ UCIS_ENABLE = yes
 LEADER_ENABLE = yes
 
 # From https://michael.stapelberg.ch/posts/2021-05-08-keyboard-input-latency-qmk-kinesis/
-# adds about 900 bytes!
+# adds about 900 bytes! TODO: move to asym_eager_defer_pk when it landed.
 DEBOUNCE_TYPE = sym_eager_pk
