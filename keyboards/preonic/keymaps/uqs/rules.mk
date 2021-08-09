eb5301bcf4150b1466b503135bc593b5c1fa83ce
Ulrich Spörlein <uqs@FreeBSD.org>
2021-08-09 17:31:50 +0200

    fighting compilers and linkers

--- keyboards/preonic/keymaps/uqs/rules.mk
+++ keyboards/preonic/keymaps/uqs/rules.mk
@@ -1,2 +1,7 @@
 LTO_ENABLE=no
 AUDIO_ENABLE=no
+ENCODER_ENABLE=no
+
+# Required under FreeBSD to find libc_nano.a
+EXTRALDFLAGS=-L/usr/local/gcc-arm-embedded-10-2020-q4-major/arm-none-eabi/lib
+USE_FPU=no
