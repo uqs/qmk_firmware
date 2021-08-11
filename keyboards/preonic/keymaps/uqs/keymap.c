01083d585fb94d90685bdb72386dfe9da4c37e2d
Ulrich Spörlein <uqs@FreeBSD.org>
2021-08-11 22:29:49 +0200

    move things around again, use tapping force hold and go back to 1ms polling

--- keyboards/preonic/keymaps/uqs/keymap.c
+++ keyboards/preonic/keymaps/uqs/keymap.c
@@ -34,20 +34,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,
      KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,
      KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,
-     /*thumb*/ LT(L_MOUSE, KC_TAB), LT_EXTD_ESC, KC_SPC,
+     /*thumb*/ LT_EXTD_ESC, KC_SPC, MO(L_MOUSE),
       /*aux*/  KC_NO,    KC_LGUI,  KC_LALT,
      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINUS,
      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-     /*thumb*/ KC_ENT, LT_NUM_BSPC, LT_FUNC_SHIFT_INS,
+     /*thumb*/ MO(L_FUNC), KC_ENT, LT_NUM_BSPC,
       /*aux*/  KC_NO,    KC_NO,    KC_NO
   ),
   [L_EXTD] = LAYOUT_uqs(
-     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,
-     _______,  WIN_PREV, TM_PREV,  KC_PGUP,  TM_NEXT,  WIN_NEXT,
-     _______,  OSM_GUI,  OSM_ALT,  OSM_SFT,  OSM_CTL,  KC_RALT,
-     _______,  ALT_TAB,  KC_SCTAB, KC_CTAB,  KC_PGDN,  LSFT(KC_INS),
+     KC_F11,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
+     KC_BTN3,  WIN_PREV, TM_PREV,  KC_PGUP,  TM_NEXT,  WIN_NEXT,
+     KC_BTN2,  OSM_GUI,  OSM_ALT,  OSM_SFT,  OSM_CTL,  KC_RALT,
+     KC_BTN1,  ALT_TAB,  KC_SCTAB, KC_CTAB,  KC_PGDN,  LSFT(KC_INS),
      /*thumb*/ _______,  _______,  _______,
       /*aux*/  _______,  _______,  _______,
                /* ^^^^ can't be used */
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_INS,   INS_HARD,
      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_DEL,   KC_NO,
      WIN_LEFT, WIN_DN,   WIN_UP,   WIN_RGHT, KC_PSTE,  KC_ENTER,  // KC_PSTE works in XTerm to emulate middle-click
-     /*thumb*/ _______,  KC_BSPC,  _______,
+     /*thumb*/ _______,  _______,  KC_BSPC,
       /*aux*/  _______,  _______,  _______
                /* ^^^^ use these */
   ),
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,
      _______,  KC_SCLN,  KC_COLN,  KC_LCBR,  KC_LPRN,  KC_LBRC,
      _______,  KC_CIRC,  KC_AMPR,  KC_RCBR,  KC_RPRN,  KC_RBRC,
-     /*thumb*/ _______,  KC_ESC,   KC_SPC,
+     /*thumb*/ KC_ESC,   KC_SPC,   _______,
       /*aux*/  _______,  _______,  _______,
                /* ^^^^ use these */
      KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F12,
@@ -96,9 +96,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L_MOUSE] = LAYOUT_uqs(
      KC_NO,    _______,  _______,  _______,  _______,  _______,
-     KC_NO,    KC_NO,    KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_NO,
-     KC_NO,    KC_NO,    KC_ACL0,  KC_ACL1,  KC_ACL2,  KC_NO,
-     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+     KC_NO,    KC_NO,    KC_BTN2,  KC_BTN3,  KC_BTN1,  KC_VOLU,
+     KC_NO,    KC_NO,    KC_ACL0,  KC_ACL1,  KC_ACL2,  KC_VOLD,
+     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_MUTE,
      /*thumb*/ _______,  _______,  _______,
       /*aux*/  _______,  _______,  _______,
                /* ^^^^ can't be used */
