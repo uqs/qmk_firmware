8bbbe615acf3a10d000d9e6f1fcf82fe5e745fcd
Ulrich Spörlein <uqs@FreeBSD.org>
2021-08-11 14:18:42 +0200

    fix preonic with gcc 10/11

--- keyboards/preonic/keymaps/uqs/rules.mk
+++ keyboards/preonic/keymaps/uqs/rules.mk
@@ -2,6 +2,13 @@ LTO_ENABLE=no
 AUDIO_ENABLE=no
 ENCODER_ENABLE=no
 
-# Required under FreeBSD to find libc_nano.a
-#EXTRALDFLAGS=-L/usr/local/gcc-arm-embedded-10-2020-q4-major/arm-none-eabi/lib
+# Broken with gcc >6, see https://github.com/qmk/qmk_firmware/issues/6753
+# (yes, different option, but I suspect the code has moved around)
+# This produces working firmware on Arch Linux with GCC 11, but is still broken
+# on Debian with 8.3.1
+RGBLIGHT_ENABLE=no
+RGB_MATRIX_ENABLE=no
+
+# not required, but helps on debian where there is a soft/hard float confusion.
+# but see above, gcc 8.3.1 still fucks up the firmware.
 USE_FPU=no
